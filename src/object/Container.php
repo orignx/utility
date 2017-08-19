9cb8e09e8e096f4724e2683c35f11124de420ca1
Nana Kwame <9naquame@gmail.com>
2017-08-19 11:24:38 +0000

    offset exist and set defined

--- src/object/Container.php
+++ src/object/Container.php
@@ -3,24 +3,31 @@
 class Container implements Countable, ArrayAccess
 {
     private $type;
+    protected $data;
     
     public function __construct($type)
     {
         $this->type =  $type;
     }
-
+    
+    public function getType($type)
+    {
+        $this->type;
+    }
+    
     public function offsetSet($offset, $value)
     {
         ;
     }
     
-    public function offsetGet($offset, $value)
+    public function offsetGet($offset)
     {
-        ;
+        return $this->data[$offset];
     }
-    public function offsetExists($offset, $value)
+    
+    public function offsetExists($offset)
     {
-        ;
+        return array_key_exists($offset, $this->data);
     }
     
     public function offsetUnset($offset, $value)
